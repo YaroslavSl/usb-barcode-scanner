706e53e9c04cf0d5ea434c160de678513b7db629
Yaroslav Slipchuk <yaroslav@smoothr.de>
2020-09-17 12:58:34 +0300

    compiled

--- dist/usb-barcode-scanner.d.ts
+++ dist/usb-barcode-scanner.d.ts
@@ -6,10 +6,10 @@ export declare class UsbScanner extends EventEmitter implements onDataScanned {
     hid?: HID;
     hidMap: any;
     constructor(options: UsbScannerOptions, hidMap?: any);
-    private retreiveDevice(vendorId, productId);
-    private retreiveDeviceByPath(path);
+    private retreiveDevice;
+    private retreiveDeviceByPath;
     startScanning(): void;
     stopScanning(): void;
-    private emitDataScanned(data);
-    private emitError(error);
+    private emitDataScanned;
+    private emitError;
 }
